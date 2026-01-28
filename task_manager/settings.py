1e184620657730406ccc6f150743d471aa4b7bba
N-Davie <102053096+N-Davie@users.noreply.github.com>
2026-01-29 00:23:31 +0500

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'postgres'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'NAME': 'task_manager',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',  # <- не localhost
+        'PORT': 5432,
     }
 }
 
